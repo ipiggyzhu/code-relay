51c01f58c0e983b54f440e6623208e71409e6bc4
piggy <ipiggyzhu@gmail.com>
2025-12-17 15:30:12 +0800

    fix: remove TransitionChild wrapper to force modal visibility

--- frontend/src/components/common/BaseModal.vue
+++ frontend/src/components/common/BaseModal.vue
@@ -3,26 +3,16 @@
     <Dialog as="div" class="mac-modal-backdrop" :open="open" @close="$emit('close')">
       <div class="mac-modal-overlay" aria-hidden="true"></div>
       <div class="mac-modal-wrapper">
-        <TransitionChild
-          as="template"
-          enter="mac-modal-enter-active"
-          enter-from="mac-modal-enter-from"
-          enter-to="mac-modal-enter-to"
-          leave="mac-modal-leave-active"
-          leave-from="mac-modal-leave-from"
-          leave-to="mac-modal-leave-to"
-        >
-          <DialogPanel :class="['mac-modal', variantClass]">
-            <header class="mac-modal-header">
-              <DialogTitle class="mac-modal-title">{{ title }}</DialogTitle>
-              <button class="ghost-icon" aria-label="Close" @click="$emit('close')">✕</button>
-            </header>
-            <div class="mac-modal-body mac-modal-scrollable">
-              <slot />
-            </div>
-            <slot name="footer" />
-          </DialogPanel>
-        </TransitionChild>
+        <DialogPanel :class="['mac-modal', variantClass]">
+          <header class="mac-modal-header">
+            <DialogTitle class="mac-modal-title">{{ title }}</DialogTitle>
+            <button class="ghost-icon" aria-label="Close" @click="$emit('close')">✕</button>
+          </header>
+          <div class="mac-modal-body mac-modal-scrollable">
+            <slot />
+          </div>
+          <slot name="footer" />
+        </DialogPanel>
       </div>
     </Dialog>
   </TransitionRoot>
